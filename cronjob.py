8b9cbc6394870272d2a7efb3e2824239652964da
Diddlin <95501222+Diddlin@users.noreply.github.com>
2021-12-03 18:15:58 -0500

    Update cronjob.py

--- cronjob.py
+++ cronjob.py
@@ -2,10 +2,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 # Main cronjob function.
-from ZillowProject import send_new_house
+from ZillowProject import send_new_house, test
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
 scheduler.add_job(send_new_house, "interval", seconds=60*60*8)
+scheduler.add_job(test, "interval", seconds=60)
+
 
 scheduler.start()
